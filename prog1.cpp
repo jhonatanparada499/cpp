9c9698be412eefa89d1770ad7c1c47572a3ff526
Jhonatan Parada <jhonatanparada499@gmail.com>
2025-12-27 15:42:59 -0500

    feat: add simple multiplication program

--- prog1.cpp
+++ prog1.cpp
@@ -1,3 +1,15 @@
+#include <iostream>
+
 int main(){
+  int n1, n2;
+  std::cout << "Enter two numbers: ";
+  std::cin >> n1;
+  std::cin >> n2;
+
+  // Multiplies two numbers
+  std::cout 
+    << n1 << " times " << n2 << " is " << n1 * n2
+    << std::endl;
+
   return 0;
 }
