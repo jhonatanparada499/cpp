9b79eae179caa91e1f254312cc95e869bd7e8674
Jhonatan Parada <jhonatanparada499@gmail.com>
2025-12-27 17:35:15 -0500

    feat: add program to sum integers from standard input

--- prog1.cpp
+++ prog1.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
 
-int main(){
-  int n1, n2;
-  std::cout << "Enter two numbers: ";
-  std::cin >> n1;
-  std::cin >> n2;
+int main()
+{
+  int sum = 0;
+  int firstNumber = 0;
+  int lastNumber = 0;
+  int val = 0;
 
-  // Multiplies two numbers
-  std::cout 
-    << n1 << " times " << n2 << " is " << n1 * n2
+  // std::cout << "Enter first number: ";
+  // std::cin >> firstNumber;
+  //
+  // std::cout << "Enter last number: ";
+  // std::cin >> lastNumber;
+  //
+  // val = lastNumber - firstNumber;
+
+  while (std::cin >> val) {
+    sum += val;
+  }
+
+  std::cout << "The sum is "
+    << sum
     << std::endl;
 
   return 0;
