9d0ae04eb7574e6eed8a0e28f25d4cf0501f7ee2
Jhonatan Parada <jhonatanparada499@gmail.com>
2025-12-30 20:32:21 -0500

    style: change identation

--- prog1.cpp
+++ prog1.cpp
@@ -1,15 +1,13 @@
-#include <iostream>
 #include "Sales_item.h"
+#include <iostream>
 
-int main()
-{
+int main() {
   Sales_item item1, item2;
   std::cin >> item1 >> item2;
   if (item1.isbn() == item2.isbn()) {
     std::cout << item1 + item2 << std::endl;
     return 0;
   }
-  std::cerr << "Data must refer to same ISBN"
-    << std::endl;
+  std::cerr << "Data must refer to same ISBN" << std::endl;
   return -1;
 }
