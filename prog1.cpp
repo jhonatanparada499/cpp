963fef607e979721279857b2ee7026d01a5c6453
Jhonatan Parada <jhonatanparada499@gmail.com>
2026-01-05 18:52:30 -0500

    feat: definition vs declaration

--- prog1.cpp
+++ prog1.cpp
@@ -1,13 +1,15 @@
-#include "Sales_item.h"
+// #include "Sales_item.h"
 #include <iostream>
+#include <string>
+// #include <iterator>
+
+std::string b;
+extern int external;
 
 int main() {
-  Sales_item item1, item2;
-  std::cin >> item1 >> item2;
-  if (item1.isbn() == item2.isbn()) {
-    std::cout << item1 + item2 << std::endl;
-    return 0;
-  }
-  std::cerr << "Data must refer to same ISBN" << std::endl;
-  return -1;
+  std::string i;
+  unsigned u1 = -1, u2 = -10;
+  std::cout << external << '\n';
+  std::cout << "Who goes with F\145rgus?\012" << std::endl;
+  return 0;
 }
