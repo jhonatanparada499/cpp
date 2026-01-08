79ab44f8d6ca42b39a00626ad918f5c197c0a951
Jhonatan Parada <jhonatanparada499@gmail.com>
2026-01-08 17:56:13 -0500

    feat: objects defined outside functions have fixed addresses that can be used fo constexpr pointers to int

--- prog1.cpp
+++ prog1.cpp
@@ -5,15 +5,18 @@
 std::string b;
 extern int external;
 
+int z = 0;
+signed &u3 = z;
+int *u4 = &u3;
+
 int main() {
   // std::string i;
   unsigned u1 = -1, u2 = -10;
-  int z = 0;
-  signed &u3 = z;
-  int *u4 = &u3;
+
+  int y = 3;
 
   void *p1 = &z;
-  int *p2 = u4;
+  constexpr int *p2 = &z;
   long *p3 = nullptr;
   double *p4 = NULL;
 
