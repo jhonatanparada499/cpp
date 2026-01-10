9c70782f4767b34db173f05201dcf60661d7ea0d
Jhonatan Parada <jhonatanparada499@gmail.com>
2026-01-09 19:29:26 -0500

    feat: add custom data definition using struct

--- prog1.cpp
+++ prog1.cpp
@@ -8,21 +8,17 @@ extern int external;
 int z = 0;
 signed &u3 = z;
 int *u4 = &u3;
+unsigned u1 = -1, u2 = -10;
+constexpr int *p2 = &z;
+const int &ri = 0;
 
 int main() {
-  // std::string i;
-  unsigned u1 = -1, u2 = -10;
-
-  int y = 3;
-
-  void *p1 = &z;
-  constexpr int *p2 = &z;
-  long *p3 = nullptr;
-  double *p4 = NULL;
-
-  double dval = 3.14;
-  const int &ri = dval;
-
   std::cout << external << std::endl;
   return 0;
 }
+
+struct Sales_data {
+  std::string bookNo;
+  unsigned unitsSold = 0;
+  double revenue;
+};
