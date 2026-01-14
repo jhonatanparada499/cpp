ab10f09cd179cf17a4b743e5f3269806e65acd41
Jhonatan Parada <jhonatanparada499@gmail.com>
2026-01-14 16:31:05 -0500

    feat: add work with iterators to loop on the characters of a string container

--- prog1.cpp
+++ prog1.cpp
@@ -1,30 +1,22 @@
 // #include "Sales_data.h"
 
-// #include <cctype>
-
+#include <cctype>
 #include <iostream>
-using std::cin;
+// using std::cin;
 using std::cout;
-//  using std::endl;
+using std::endl;
 
 #include <string>
 using std::string;
 
-#include <vector>
-using std::vector;
+// #include <vector>
+// using std::vector;
 
 int main() {
-  vector<unsigned> scores(11, 0);
-
-  unsigned grade;
-  while (cin >> grade) {
-    if (grade <= 100) {
-      scores[grade / 10]++;
-    }
-  }
-
-  for (unsigned grade : scores) {
-    cout << grade << " ";
+  const string my_string("Hello World");
+  for (auto i = my_string.begin(); i != my_string.end() && !isspace(*i); ++i) {
+    //*i = toupper(*i);
+    cout << *i << endl;
   }
 
   return 0;
