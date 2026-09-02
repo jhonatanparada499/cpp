ade067315ea0d31c698f5f0c5c5ce6198f1cc061
jhonatanparada499 <jhonatanparada499@gmail.com>
2026-09-02 08:22:21 -0400

    refact: change param types

--- fall_2026/6-26.cpp
+++ fall_2026/6-26.cpp
@@ -7,6 +7,12 @@ void increment(int num){
   num++;
 }
 
+void swapNum(int& num1, int& num2){
+  int temp = num1;
+  num2 = num1;
+  num1 = temp;
+}
+
 int main(){
   int x = 5;
   cout << "Before: " << x << endl;
